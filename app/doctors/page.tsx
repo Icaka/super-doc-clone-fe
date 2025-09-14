932edbbc6a4ca52d31db203223a0a69f9da37795
Icaka <ico.ezekiev@gmail.com>
2025-09-14 20:19:04 +0300

    fetching doctors from back-end

--- app/doctors/page.tsx
+++ app/doctors/page.tsx
@@ -1,16 +1,11 @@
-export default function DoctorList() {
-    const Doctors = [{
-        firstName: "Ivan",
-        lastName: "Draganov"
-    }, {
-        firstName: "John",
-        lastName: "Doe"
-    }]
+export default async function DoctorList() {
+    const doctorsData = await fetch("http://localhost:8080/doctors")
+    const doctors = await doctorsData.json()
 
     return(
         <div>
-            {Doctors.map(d=>
-                <p>{d.firstName} {d.lastName}</p>)
+            {doctors.map((d:any)=>
+                <p key={d.id}>{d.firstName} {d.lastName}</p>)
             }
         </div>
 
