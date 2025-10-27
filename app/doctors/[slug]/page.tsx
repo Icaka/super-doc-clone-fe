4c6e61ec38d615705408ca0ca6d6bc1dd96bae9c
Icaka <ico.ezekiev@gmail.com>
2025-10-27 15:15:43 +0200

    change to client component (hooks)

--- app/doctors/[slug]/page.tsx
+++ app/doctors/[slug]/page.tsx
@@ -1,24 +1,39 @@
 "use client"
 import Timestamp from "react-timestamp";
+import {Doctor as DoctorModel, Review} from "@/api/models"
 import {apiClient} from "@/api/client";
+import {useEffect, useState} from "react";
+import {useParams} from "next/navigation";
 
-export default async function Doctor({
-                                        params,
-                                   }: {
-    params: Promise<{ slug: string }>
-}) {
-    const { slug } = await params
-    const doctor = await apiClient.getDoctor(slug);
-    const reviews = await apiClient.getDoctorReviews(slug);
+export default function Doctor() {
+    const params = useParams<{slug: string}>()
+    const [doctor, setDoctor] = useState<DoctorModel | null>(null)
+    useEffect(() => {
+        async function fetchDoctor() {
+            const doctorFetch = await apiClient.getDoctor(params.slug);
+            setDoctor(doctorFetch);
+        }
+        fetchDoctor()
+    }, [params])
+
+    const [reviews, setReviews] = useState<Review[] | null>(null)
+    useEffect(() => {
+        async function fetchReviews(){
+            const reviewsFetch = await apiClient.getDoctorReviews(params.slug);
+            setReviews(reviewsFetch)
+        }
+        fetchReviews();
+    }, [params]);
+
     return(
         <div className="p-5">
-            <h1 className={"text-2xl font-bold"}>{doctor.firstName} {doctor.lastName}<br/>
-            Age: <Timestamp relative date={doctor.dateOfBirth} relativeTo={Date} /></h1>
+            <h1 className={"text-2xl font-bold"}>{doctor?.firstName} {doctor?.lastName}<br/>
+            Age: <Timestamp relative date={doctor?.dateOfBirth??Date.now()} relativeTo={Date} /></h1>
             <div style={{border: "dotted"}}>
                 <h2 className={"text-1xl font-bold"}>Reviews:</h2>
                 <ul className="list-disc pl-5 ml-5">
-                    {reviews.map((re:any)=>
-                        <li><p key={re.id}>{re.text}</p></li>
+                    {reviews?.map((re:any)=>
+                        <li key={re.id}>{re.text}</li>
                     )}
                 </ul>
             </div>
